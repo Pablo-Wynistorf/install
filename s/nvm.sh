cf4a5fff1bccb47c5bf92cc972d17650f8a8d1d0
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 15:53:49 +0200

    updated nvm install

--- s/nvm.sh
+++ s/nvm.sh
@@ -3,4 +3,8 @@
 LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${LATEST_VERSION}/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 nvm --version
