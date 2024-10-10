3f16fb1f46e72ede93031521c7b50222ebfdd9ce
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 16:38:22 +0200

    updated nvm

--- s/nvm.sh
+++ s/nvm.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-NVM_DIR="/usr/local/nvm"
-
 LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${LATEST_VERSION}/install.sh | bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${LATEST_VERSION}/install.sh | NVM_DIR=$NVM_DIR bash
-
-echo "export NVM_DIR=\"$NVM_DIR\"" | sudo tee -a /etc/profile
-echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" | sudo tee -a /etc/profile
-echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"" | sudo tee -a /etc/profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm --version
-
-echo "NVM has been installed. Please run 'source /etc/profile' or restart your terminal to use it."
-
