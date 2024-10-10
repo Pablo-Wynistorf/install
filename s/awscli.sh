a34a8f1b93a2d674fd028f0c8908968cf59719a0
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 14:27:38 +0200

    added aws cli and index.html

---
+++ s/awscli.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+if ! [ -x "$(command -v unzip)" ]; then
+  echo 'Info: unzip is not installed.' >&2
+  echo 'Trying to install unzip...'
+  sudo apt-get install unzip -y
+  if ! [ -x "$(command -v unzip)" ]; then
+    echo 'Error: unzip install faild' >&2
+    exit 1
+  fi
+fi
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws
