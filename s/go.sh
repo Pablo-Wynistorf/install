fb1b6efe7964b6724676ceccd6bfd6cf9d721c6a
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 16:37:23 +0200

    updated go

--- s/go.sh
+++ s/go.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-GO_INSTALL_DIR="/usr/local/go"
-
-curl -LO https://golang.org/dl/$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].files[] | select(.os == "'$(uname -s)'" and .arch == "'$(uname -m)'") | .filename')
-
-GO_TAR=$(basename $(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].files[] | select(.os == "'$(uname -s)'" and .arch == "'$(uname -m)'") | .filename'))
-
-tar -C /usr/local -xzf $GO_TAR
-rm $GO_TAR
-
-echo "export PATH=\$PATH:$GO_INSTALL_DIR/bin" >> /etc/profile.d/go.sh
-chmod +x /etc/profile.d/go.sh
-
-echo "Go has been installed successfully. Please restart your terminal or run 'source /etc/profile.d/go.sh' to update your PATH."
+curl -LO get.golang.org/$(uname)/go_installer
+chmod +x go_installer
+./go_installer --version $(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
+rm go_installer
