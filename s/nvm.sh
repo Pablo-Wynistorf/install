d217a4202d873d9de1fe72f965c7af5e25483cb5
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 16:35:24 +0200

    updated go and nvm

--- s/nvm.sh
+++ s/nvm.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${LATEST_VERSION}/install.sh | bash
+NVM_DIR="/usr/local/nvm"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${LATEST_VERSION}/install.sh | NVM_DIR=$NVM_DIR bash
+
+echo "export NVM_DIR=\"$NVM_DIR\"" | sudo tee -a /etc/profile
+echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" | sudo tee -a /etc/profile
+echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"" | sudo tee -a /etc/profile
 
 nvm --version
+
+echo "NVM has been installed. Please run 'source /etc/profile' or restart your terminal to use it."
+
