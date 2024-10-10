7e5e4803d4779dce5763828e9f0c7e3c93e0cf4b
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 15:52:30 +0200

    updated go install

--- s/go.sh
+++ s/go.sh
@@ -2,5 +2,5 @@
 
 sudo curl -LO get.golang.org/$(uname)/go_installer
 sudo chmod +x go_installer
-sudo ./go_installer --version $(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
+./go_installer --version $(curl https://go.dev/dl/?mode=json | jq -r '.[0].version')
 sudo rm go_installer
