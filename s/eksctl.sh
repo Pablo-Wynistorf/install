9bb66c63733865ecffa3f7735a6e84d69a3255fe
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2024-10-10 16:54:41 +0200

    updated eksctl

--- s/eksctl.sh
+++ s/eksctl.sh
@@ -4,13 +4,14 @@ ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt"
 
-if [ $? -ne 0 ]; then
+if [ "grep $PLATFORM eksctl_checksums.txt | sha256sum --check" ]; then
   echo "eksctl checksum failed"
   exit 1
 fi
 
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-
 sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version
